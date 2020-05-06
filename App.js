8cec0e9093fa09d29bf18ac4bb8af95cf33f01df
hojunlee77 <54741551+hojunlee77@users.noreply.github.com>
2020-05-06 23:36:49 +0900

    1.1 cacheImages

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 import { AppLoading } from "expo";
+import { Image } from "react-native";
+import { Asset } from "expo-asset";
+
+const cacheImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 
 export default function App() {
-  return <AppLoading />;
+  const [isReady, setisReady] = useState(false);
+  const loadAssets = async () => {
+    const images = cacheImages([
+      "https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      require("./assets/splash.png"),
+    ]);
+    console.log(images);
+  };
+  const onFinish = () => setisReady(true);
+  return isReady ? null : (
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish}
+      onError={console.error}
+    />
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
